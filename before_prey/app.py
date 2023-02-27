e7aa73745ecfe38dbc874514340f277976a849b5
pcw999 <cksdnelly@gmail.com>
2023-02-27 21:08:43 +0900

    50

--- before_prey/app.py
+++ before_prey/app.py
@@ -432,7 +432,7 @@ class SnakeGameClass:
         b = 0
         test_code = str(sid)
 
-        for i in range(20):
+        for i in range(50):
             if i % 2 == 0:
                 test_code = str(sid)
             self.sock.sendto(test_code.encode(), self.opp_addr)
